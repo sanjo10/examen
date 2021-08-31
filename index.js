507364b83a9a8dee3e0e3b5983c22d830c4e8f44
sanjo10 <68831540+sanjo10@users.noreply.github.com>
2021-08-31 09:54:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var cliente= function(){
+    var Cedula = document.getElementById("Cedula").value;
+    var Apellidos = document.getElementById("Apellidos").value;
+    var Nombres = document.getElementById("Nombres").value;
+    var Direccion = document.getElementById("Dieccion").value;
+    var Paralelo = document.getElementById("Telefono").value;
+    var Correo = document.getElementById("Correo").value;
+
+    if (Cedula == "") {
+    document.getElementById("Cedula").focus();
+    } else {
+    if (Apellidos == "") {
+    document.getElementById("Apellidos").focus();
+    } else {
+    if (Nombres == "") {
+    document.getElementById("Nombres").focus();
+    } else {
+    if (Direccion == "") {
+    document.getElementById("Direccion").focus();
+    } else {
+        if (Telefono == "") {
+            document.getElementById("Telefono").Value();
+        } else {
+    if (Correo == "") {
+    document.getElementById("Correo").focus();
+    } else {
+    console.log(Cedula+" "+Apellidos+" "+Nombres+" "+Direccion+" "+Telefono+"  "+Correo);
+    document.getElementById("Cedula").value = "";
+    document.getElementById("Apellidos").value = "";
+    document.getElementById("Nombres").value = "";
+    document.getElementById("Dirrecion").value = "";
+    document.getElementById ( "Telefono" ). value ()    
+    document.getElementById ( "Correo" ). focus ()      
+    }
+}
+}
+}
+}
+} 
+}
+ 
